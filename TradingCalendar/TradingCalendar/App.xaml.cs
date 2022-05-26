22fcb95c6fab19c9b9bd74aaf78b54d7c58ae817
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-05-26 11:53:58 +0200

    initial commit (#1) (#2)

---
+++ TradingCalendar/TradingCalendar/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TradingCalendar;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TradingCalendar/TradingCalendar/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TradingCalendar;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
