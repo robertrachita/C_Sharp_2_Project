233d050f8b4b147b6dc23b9df05dd586338bb021
christostevan <72568727+christostevan@users.noreply.github.com>
2022-07-05 07:16:51 +0100

    update login

--- TradingCalendar/TradingCalendar/MainPage.xaml.cs
+++ TradingCalendar/TradingCalendar/MainPage.xaml.cs
@@ -1,8 +1,5 @@
 ﻿namespace TradingCalendar;
 
-using Microsoft.EntityFrameworkCore;
-using System.Data.SqlClient;
-using TradingCalendar;
 public partial class MainPage : ContentPage
 {
 
@@ -15,16 +12,6 @@ public partial class MainPage : ContentPage
 	private void Button_Clicked(object sender, EventArgs e)
 	{
 		Shell.Current.GoToAsync(nameof(RegisterPage));
-    }
-}
-
-public class Database : DbContext
-{
-    private const string conn = $"Server=oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com; Database=rnjhdkckwifts8kj; User Id=f5ytxsgeiwojfsy7; Password=q20j9i15pgyyoi3x";
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(conn);
     }
 }
 
